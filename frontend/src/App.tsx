b053b09a7edd293d9d47d1d9261b5d6f9b532918
Ugochukwu Nebolisa <nebolisaugochukwu@gmail.com>
2026-05-11 22:50:17 +0100

    feat: define app routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/login";
+import AppLayout from "./components/layouts/app-layout";
+import DashboardPage from "./pages/dashboard";
+import RoomsPage from "./pages/rooms";
 
 function App() {
   return (
@@ -7,6 +10,10 @@ function App() {
       <Routes>
         <Route index element={<Navigate to="/login" />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route element={<AppLayout />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/rooms" element={<RoomsPage />} />
+        </Route>
       </Routes>
     </>
   );
